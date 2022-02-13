eaa10d0f8bc118279b330486a1dafcf4b3f3d92c
goldspoon0519 <98209165+goldspoon0519@users.noreply.github.com>
2022-02-14 07:27:16 +0900

    Update blockchainActions.js

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -49,7 +49,7 @@ export const connect = () => {
     });
     const CONFIG = await configResponse.json();
     const { ethereum } = window;
-    const metamaskIsInstalled = ethereum && ethereum.klaytn;
+    const metamaskIsInstalled = ethereum && ethereum.isMetaMask;
     if (metamaskIsInstalled) {
       Web3EthContract.setProvider(ethereum);
       let web3 = new Web3(ethereum);
