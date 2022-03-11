baf1bba443e882c95e2f9568385183c3b224e310
goldspoon0519 <98209165+goldspoon0519@users.noreply.github.com>
2022-03-12 00:33:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
+                  window.open("https://goldpolarbear.net/bbs/board.php?bo_table=introduction", "_blank");
                 }}
                 style={{
                   margin: "5px",
